81709a40be072e81f06612d307ec9d6dc207dd8a
snehacontinuum <sneha.chemburkar@continuum.net>
2016-12-23 12:47:19 +0530

    updated test case extracting logic

--- ITSWebserviceAutomation/src/main/java/continuum/SoapUIIntegrator.java
+++ ITSWebserviceAutomation/src/main/java/continuum/SoapUIIntegrator.java
@@ -2,6 +2,7 @@ package continuum;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,135 +35,146 @@ import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
 
 
-public class SoapUIIntegrator {
-	
-	/**************
-	 * Integrating soap UI with java
-	 * @author sneha.chemburkar
-	 * @return 
-	 * 	 
-	 */
-	
-	
-	public static final SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
-	static //static TestRunner runner1=null;
-	List<TestCase> testCaseList;
-	
-	static String apiFilePath = new File("").getAbsolutePath()+"\\src\\test\\ReadyAPIProjectFiles\\";
-	
-	public static void setProjectFile(String soapuiProjectPath)  {
-    	String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
-	
-		System.out.println("Soapui Project Path="+readyAPIProjectPath);
-			
-		runner.setProjectFile(readyAPIProjectPath);
-		
-		runner.setJUnitReport(true);
-	   	runner.setOutputFolder("target/surefire-reports");
-		runner.setIgnoreError(true);
-		
-	}
-	public static String[][] getTestCases(String soapuiProjectPath){
-		String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
-		String [][]testCase =null;
-		WsdlProject project;
-		
-		try {
-			project = new WsdlProject(readyAPIProjectPath);
-			 			  
-		    
-		   // retrieve all test suite from project
-		    List<TestSuite> testSuiteList = project.getProject().getTestSuiteList();
-		    
-		    // Iterate all TestSuites of project
-		    for (TestSuite ts : testSuiteList) {
-		    	 Reporter.log("****Running Test suite " + ts.getName() + "********");
-		    	 System.out.println("****Running Test suite " + ts.getName() + "********");                         
-		         // Retrieve all TestCases from a particular TestSuite
-		         testCaseList = ts.getTestCaseList();
-		         testCase = new String[testCaseList.size()][1];                                 
-		          // Iterate all TestCases of the particular TestSuite
-		           for (int i=0;i<testCaseList.size();i++)
-		           {
-		        	   Reporter.log("****Running Test case " + testCaseList.get(i).getName() + "********");
-		        	   System.out.println("****Running Test case " + testCaseList.get(i).getName() + "********");
-		        	   testCase[i][0]=	 testCaseList.get(i).getName() ;              
-		              
-		              }
-		       }
-			
-		   } catch (XmlException | IOException | SoapUIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		   }
-			return testCase;
-	}
-	
-	
-	public static void excuteSoapUI(String testCase)
-	{
-		System.out.println("****Running Test case " + testCase+ "********");
-		try {
-			runner.setTestCase(testCase);
-			runner.run();		
-			
-			
-		} catch (Exception e) {
-			System.out.println("Error while executing Ready Api Project");
-			e.printStackTrace();
-		} 
-	}
-	
-	public static Map<String, String> getReport(String soapuiProjectPath)
-	{
-	  	
-	  	String flag="passed";
-	  	List <TestCase> testcaseFailed=runner.getFailedTests();
-		String [][]tc=getTestCases(soapuiProjectPath);
-			Map<String,String> results=new HashMap<String,String>();
-			 for(int j=0;j<tc.length;j++)
-             {
-		               for(int i=0;i<testcaseFailed.size();i++)
-		                {
-		                 if(tc[j][0].equalsIgnoreCase(testcaseFailed.get(i).getName()))
-		                 {
-		 					 flag="failed";
-		                 }
-		            
-		                }
-		               results.put(tc[j][0],flag);
-		              
-		       }
- 
-		       return results;
-		    }
-	
-	
-	
-	
-	
-	
-		
-
-	
-		  
-//	public static void testAPI(String soapuiProjectPath,String testCase)  {
-//		String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
-//		//String readyAPIProjectPath=soapuiProjectPath;
-//		System.out.println("Soapui Project Path="+readyAPIProjectPath);
-//			
-//		runner.setProjectFile(soapuiProjectPath);
-//		runner.setTestCase(testCase);
-//		runner.setJUnitReport(true);
-//		runner.setOutputFolder("target/surefire-reports");
-//		runner.setIgnoreError(true);
-//
-//	}
 
 	
 
-		
-		
+
+	public class SoapUIIntegrator {
+	 
+	 /**************
+	  * Integrating soap UI with java
+	  * @author sneha.chemburkar
+	  * @return 
+	  *   
+	  */
+	 
+	 
+	 public static final SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
+	 static //static TestRunner runner1=null;
+	 List<TestCase> testCaseList;
+	 
+	 static String apiFilePath = new File("").getAbsolutePath()+"\\src\\test\\ReadyAPIProjectFiles\\";
+	 
+	 public static void setProjectFile(String soapuiProjectPath)  {
+	     String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
+	 
+	  System.out.println("Soapui Project Path="+readyAPIProjectPath);
+	   
+	  runner.setProjectFile(readyAPIProjectPath);
+	  
+	  runner.setJUnitReport(true);
+	     runner.setOutputFolder("target/surefire-reports");
+	  runner.setIgnoreError(true);
+	  
+	 }
+	 
+	 public static String[][] getTestCases(String soapuiProjectPath) {
+	  String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
+	  WsdlProject project;
+	 
+	  List<String> testCase=new ArrayList<String>();
+	  try {
+	   project = new WsdlProject(readyAPIProjectPath);
+	     // retrieve all test suite from project
+	      List<TestSuite> testSuiteList = project.getProject().getTestSuiteList();
+	      
+	      // Iterate all TestSuites of project
+	      for (TestSuite ts : testSuiteList) {
+	                               
+	           // Retrieve all TestCases from a particular TestSuite
+	           testCaseList = ts.getTestCaseList();
+	           for (TestCase tc : testCaseList) {
+	       //  System.out.println("test case size"+testCaseList.size());
+	            // Iterate all TestCases of the particular TestSuite
+	           
+	             
+	              //Reporter.log("****Running Test case " + testCaseList.get(i).getName() + "********");
+	             System.out.println("****Running Test case " + tc.getName() + "********");
+	              testCase.add(tc.getName());              
+	                }
+	         }
+	   
+	     } catch (XmlException | IOException | SoapUIException e) {
+	   // TODO Auto-generated catch block
+	   e.printStackTrace();
+	     }
+	     String[][] testCaseObject=new String[testCase.size()][1];
+	     for(int i=0;i<testCase.size();i++)
+	     {
+	         testCaseObject[i][0]=testCase.get(i).toString();
+	     }
+	      return testCaseObject;
+	 }
+	 
+	 
+	 public static void excuteSoapUI(String testCase)
+	 {
+	  System.out.println("****Executing Test case " + testCase+ "********");
+	  Reporter.log("****Executing Test case " + testCase+ "********");
+	  try {
+	   runner.setTestCase(testCase);
+	   runner.run();  
+	   
+	   
+	  } catch (Exception e) {
+	   System.out.println("Error while executing Ready Api Project");
+	   e.printStackTrace();
+	  } 
+	 }
+	 
+	 public static Map<String, String> getReport(String soapuiProjectPath) 
+	 {
+	    
+	    String flag="passed";
+	    List <TestCase> testcaseFailed=runner.getFailedTests();
+	  String [][]tc=getTestCases(soapuiProjectPath);
+	   Map<String,String> results=new HashMap<String,String>();
+	    for(int j=0;j<tc.length;j++)
+	             {
+	                 for(int i=0;i<testcaseFailed.size();i++)
+	                  {
+	                   if(tc[j][0].equalsIgnoreCase(testcaseFailed.get(i).getName()))
+	                   {
+	         flag="failed";
+	                   }
+	              
+	                  }
+	                 results.put(tc[j][0],flag);
+	                
+	         }
+	 
+	         return results;
+	      }
+	 
+	 
+	 
+	 
+	 
+	 
+	  
+
+	 
+	    
+	// public static void testAPI(String soapuiProjectPath,String testCase)  {
+	//  String readyAPIProjectPath=apiFilePath+soapuiProjectPath;
+	//  //String readyAPIProjectPath=soapuiProjectPath;
+	//  System.out.println("Soapui Project Path="+readyAPIProjectPath);
+	//   
+	//  runner.setProjectFile(soapuiProjectPath);
+	//  runner.setTestCase(testCase);
+	//  runner.setJUnitReport(true);
+	//  runner.setOutputFolder("target/surefire-reports");
+	//  runner.setIgnoreError(true);
+	//
+	// }
+
+	 
+
+	  
+	  
+	 
+	 
+	
 	
 	
 }
